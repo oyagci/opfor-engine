5e90f76102611d3720c8e7f20a64f246bfd4afd0
Oguzhan YAGCI <oyagci@student.42.fr>
2020-07-04 14:24:04 +0200

    Refactor DevScene

--- src/engine/DevScene.hpp
+++ src/engine/DevScene.hpp
@@ -17,6 +17,10 @@ class DevScene : public Scene
 {
 	unsigned int _MeshShader;
 
+	ecs::IEntityBase *_PlayerCamera;
+	ecs::IEntityBase *_PointLight;
+	ecs::IEntityBase *_Wall;
+
 public:
 	DevScene()
 	{
@@ -35,45 +39,62 @@ public:
 		_MeshShader = shaderId;
 	}
 
-	void Play() override
+	void Setup() override
 	{
 		ECS().SystemManager->InstantiateSystem<MeshRendererSystem>();
 		ECS().SystemManager->InstantiateSystem<LuaSystem>();
 		ECS().SystemManager->InstantiateSystem<CameraMovementSystem>();
 
-		auto plCam = ECS().EntityManager->CreateEntity<PlayerCameraComponent, TransformComponent>();
-		auto &camera = plCam->Get<PlayerCameraComponent>();
+		// Point Light
+		// ===========
+
+		_PointLight = ECS().EntityManager->CreateEntity<PointLightComponent, TransformComponent>();
+		auto &lightProps = _PointLight->Get<PointLightComponent>();
+			lightProps.Color = { 1.0f, 1.0f, 0.8f };
+			lightProps.Intensity = 5000.0f;
+		_PointLight->Get<TransformComponent>().position = { 100.0f, 100.0f, 100.0f };
+		_PointLight->SetName("Point Light");
+
+		// Camera
+		// ======
+
+		_PlayerCamera = ECS().EntityManager->CreateEntity<PlayerCameraComponent, TransformComponent>();
+		auto &camera = _PlayerCamera->Get<PlayerCameraComponent>();
 			camera.projection = glm::perspective(glm::radians(90.0f), 16.0f / 9.0f, 0.1f, 1000.0f);
 			camera.model = glm::mat4(1.0f);
 			camera.view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
 			camera.viewProjection = camera.projection * camera.view;
 			camera.exposure = 2.0f;
-		plCam->Get<TransformComponent>().position = { 0.0f, 0.0f, 0.0f };
-		plCam->SetName("Editor Camera");
+		_PlayerCamera->Get<TransformComponent>().position = { 0.0f, 0.0f, 0.0f };
+		_PlayerCamera->SetName("Editor Camera");
 
-		auto myLight = ECS().EntityManager->CreateEntity<PointLightComponent, TransformComponent>();
-		auto &lightProps = myLight->Get<PointLightComponent>();
-			lightProps.Color = { 1.0f, 1.0f, 0.8f };
-			lightProps.Intensity = 5000.0f;
-		myLight->Get<TransformComponent>().position = { 100.0f, 100.0f, 100.0f };
-		myLight->SetName("Point Light");
-
-		auto myMesh = ECS().EntityManager->CreateEntity<ModelComponent, TransformComponent>();
-		myMesh->SetName("Wall");
-
-		auto model = myMesh->Get<ModelComponent>();
-		auto &trans = myMesh->Get<TransformComponent>();
-
-		trans.position = { 0.0f, 0.0f, 0.0f };
-		trans.scale = { 1.0f, 1.0f, 1.0f };
+		// Wall
+		// ====
 
 		engine::Model engineModel;
 		engineModel.LoadFromGLTF("models/42Run/MapTiles/Wall-1.gltf");
 
+		_Wall = ECS().EntityManager->CreateEntity<ModelComponent, TransformComponent>();
+
+		auto &trans = _Wall->Get<TransformComponent>();
+			trans.position = { 0.0f, 0.0f, 0.0f };
+			trans.scale = { 1.0f, 1.0f, 1.0f };
+
+		auto model = _Wall->Get<ModelComponent>();
 		auto &meshes = engineModel.GetMeshes();
-		model.Meshes.insert(model.Meshes.begin(), meshes.begin(), meshes.end());
-		model.Path = "models/42Run/MapTiles/Wall-1.gltf";
-		model.Shader = _MeshShader;
-		myMesh->Set(model);
+			model.Meshes.insert(model.Meshes.begin(), meshes.begin(), meshes.end());
+			model.Path = "models/42Run/MapTiles/Wall-1.gltf";
+			model.Shader = _MeshShader;
+
+		_Wall->Set(model);
+		_Wall->SetName("Wall");
+	}
+
+	void Play() override
+	{
+		_PointLight->AddComponents<LuaScriptComponent>();
+		auto &lightScript = _PointLight->Get<LuaScriptComponent>();
+			lightScript.Path = "scripts/custom_script.lua";
+			lightScript.Runtime.Load("scripts/custom_script.lua");
 	}
 };
