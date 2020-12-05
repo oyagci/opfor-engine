ca074dba9cd4b4aed52c455d5575e58f186cf2ee
Oguzhan YAGCI <oyagci@pm.me>
2020-12-05 20:14:04 +0100

    Add name field to transform component

--- opfor/src/components/TransformComponent.hpp
+++ opfor/src/components/TransformComponent.hpp
@@ -6,6 +6,8 @@
 
 struct TransformComponent : ecs::IComponentBase
 {
+	opfor::String name;
+
 	glm::vec3 position{ 0.0f, 0.0f, 0.0f };
 	glm::quat rotation{ 1.0f, 0.0f, 0.0f, 0.0f };
 	glm::vec3 scale{ 1.0f, 1.0f, 1.0f };
