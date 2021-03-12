83e7f39b4b4269be333cf4206e2663b8cc1436e3
Oguzhan YAGCI <oyagci@pm.me>
2021-03-12 10:14:20 +0100

    TransformComponent: Add reference to the parent entity

--- opfor/src/opfor/ecs/Entity.cpp
+++ opfor/src/opfor/ecs/Entity.cpp
@@ -1,8 +1,12 @@
 #include "Entity.hpp"
+#include "components/TransformComponent.hpp"
 
 namespace ecs
 {
 
 unsigned int IEntityBase::NextEntityID = 0;
+
+template <>
+void IEntityBase::Set<TransformComponent>(TransformComponent const &data) = delete;
 
 }
