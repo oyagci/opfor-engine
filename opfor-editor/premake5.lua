739264674b7ea958292cc36ef9eedb244140612b
Oguzhan YAGCI <oyagci@pm.me>
2021-03-21 16:04:09 +0100

    build: Use workspace location for output

--- opfor-editor/premake5.lua
+++ opfor-editor/premake5.lua
@@ -4,8 +4,8 @@ project "opfor-editor"
     cppdialect "C++17"
     staticruntime "On"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
     files
     {
